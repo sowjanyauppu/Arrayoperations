df81ad29cf8e1e2949a421b58c3ce8a90954ca09
sowjanyauppu <108324501+sowjanyauppu@users.noreply.github.com>
2022-07-25 17:51:21 +0530

    Add files via upload

---
+++ 36-power of a number.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int base = 3, exponent = 4;  
+long result = 1;  
+for (; exponent!= 0; --exponent)   
+{  
+result = result * base;  
+}  
+	}
+}
